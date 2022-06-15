effddf20e13b36496006ed9758353f68af018dfb
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-06-16 07:57:12 +0900

    Add functions and todos field

--- app/src/main/java/com/jp_funda/realmtodo/MainViewModel.kt
+++ app/src/main/java/com/jp_funda/realmtodo/MainViewModel.kt
@@ -1,9 +1,42 @@
 package com.jp_funda.realmtodo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val todoRepository: TodoRepository) : ViewModel() {
+    private val _todos = MutableLiveData<List<Todo>>()
+    val todo: LiveData<List<Todo>> = _todos
+
+    private fun refreshTodos() {
+        viewModelScope.launch {
+            _todos.value = todoRepository.getAllTodo()
+        }
+    }
+
+    fun addTodo(todo: Todo) {
+        viewModelScope.launch {
+            todoRepository.createTodo(todo)
+            refreshTodos()
+        }
+    }
+
+    fun updateTodo(todo: Todo) {
+        viewModelScope.launch {
+            todoRepository.createTodo(todo)
+            refreshTodos()
+        }
+    }
+
+    fun deleteTodo(todo: Todo) {
+        viewModelScope.launch {
+            todoRepository.deleteTodo(todo)
+            refreshTodos()
+        }
+    }
 }
