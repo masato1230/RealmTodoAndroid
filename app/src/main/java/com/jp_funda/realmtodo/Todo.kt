8797672e0ab40c809941699b9fdf207c0e6d5d10
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-06-16 07:26:17 +0900

    Add CRUD for Todo

--- app/src/main/java/com/jp_funda/realmtodo/Todo.kt
+++ app/src/main/java/com/jp_funda/realmtodo/Todo.kt
@@ -1,8 +1,10 @@
 package com.jp_funda.realmtodo
 
 import io.realm.kotlin.types.RealmObject
+import java.util.*
 
 class Todo(
+    var id: UUID? = UUID.randomUUID(),
     var title: String,
     var description: String,
 ) : RealmObject
--- app/src/main/java/com/jp_funda/realmtodo/TodoRepository.kt
+++ app/src/main/java/com/jp_funda/realmtodo/TodoRepository.kt
@@ -2,6 +2,8 @@ package com.jp_funda.realmtodo
 
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.ext.query
+import io.realm.kotlin.query.RealmResults
 import javax.inject.Inject
 
 class TodoRepository @Inject constructor() {
@@ -10,5 +12,42 @@ class TodoRepository @Inject constructor() {
         val config = RealmConfiguration.Builder(schema = setOf(Todo::class))
             .build()
         return Realm.open(config)
+    }
+
+    /** Create. */
+    suspend fun createTodo(todo: Todo) {
+        getRealmInstance().apply {
+            write { copyToRealm(todo) }
+        }.close()
+    }
+
+    /** Read. */
+    suspend fun getAllTodo(): RealmResults<Todo> {
+        getRealmInstance().apply {
+            val todos = query<Todo>().find()
+            close()
+            return todos
+        }
+    }
+
+    /** Update. */
+    suspend fun updateTodo(todo: Todo) {
+        getRealmInstance().apply {
+            write {
+                query<Todo>("id == $0", todo.id).find().first().apply {
+                    title = todo.title
+                    description = todo.description
+                }
+            }
+        }.close()
+    }
+
+    /** Delete. */
+    suspend fun deleteTodo(todo: Todo) {
+        getRealmInstance().apply {
+            write {
+                delete(query<Todo>("id == $0", todo.id).find())
+            }
+        }.close()
     }
 }
