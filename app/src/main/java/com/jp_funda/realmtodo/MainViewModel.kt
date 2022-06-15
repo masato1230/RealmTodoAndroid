e24fb994806d3952179379e8bfbe0ac482ea7851
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-06-16 07:42:31 +0900

    Add MainViewModel base

---
+++ app/src/main/java/com/jp_funda/realmtodo/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.jp_funda.realmtodo
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(private val todoRepository: TodoRepository) : ViewModel() {
+}
