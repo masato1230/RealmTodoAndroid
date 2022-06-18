d492a63dacc4f593887bf10611532d5b8cd4596f
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-06-19 07:18:52 +0900

    Add EditDialog

--- app/src/main/java/com/jp_funda/realmtodo/MainActivity.kt
+++ app/src/main/java/com/jp_funda/realmtodo/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.jp_funda.realmtodo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.jp_funda.realmtodo.ui.theme.RealmTodoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -30,15 +35,60 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainContent() {
+    val isShowDialog = remember { mutableStateOf(false) }
+
     Scaffold(
         floatingActionButton = {
-            FloatingActionButton(onClick = { /*TODO*/ }) {
+            FloatingActionButton(onClick = { isShowDialog.value = true }) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "追加ボタン")
             }
         }
     ) {
-
+        if (isShowDialog.value) {
+            EditDialog(isShowDialog)
+        }
     }
+}
+
+@Composable
+fun EditDialog(isShowDialog: MutableState<Boolean>) {
+    AlertDialog(
+        onDismissRequest = { isShowDialog.value = false },
+        title = { Text(text = "Todo新規作成") },
+        text = {
+            Column {
+                Text(text = "タイトル")
+                TextField(value = "", onValueChange = { /* Todo */ })
+                Text(text = "詳細")
+                TextField(value = "", onValueChange = { /* Todo */ })
+            }
+        },
+        buttons = {
+            Row(
+                modifier = Modifier.padding(all = 8.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Spacer(modifier = Modifier.weight(1f))
+                Button(
+                    modifier = Modifier.width(120.dp),
+                    onClick = { isShowDialog.value = false },
+                ) {
+                    Text(text = "キャンセル")
+                }
+                Spacer(modifier = Modifier.width(10.dp))
+                Button(
+                    modifier = Modifier.width(120.dp),
+                    onClick = {
+                        isShowDialog.value = false
+                        /* TODO todoの追加 */
+                    },
+                ) {
+                    Text(text = "OK")
+                }
+            }
+        }
+    )
 }
