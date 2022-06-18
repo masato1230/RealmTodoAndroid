c72293d1d24c2d7bfdfe995d8f0dc977a756c30d
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-06-19 07:06:44 +0900

    Add floating action button

--- app/src/main/java/com/jp_funda/realmtodo/MainActivity.kt
+++ app/src/main/java/com/jp_funda/realmtodo/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.jp_funda.realmtodo.ui.theme.RealmTodoTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,8 +23,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    MainContent()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "追加ボタン")
+            }
+        }
+    ) {
+
     }
 }
